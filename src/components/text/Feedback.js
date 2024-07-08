ae38f402942322128f2e0c2e0a8bd1b189df6cfb
bhcha <oneforall88@gmail.com>
2024-07-08 23:11:49 +0900

    add : feedback 기능 추가

--- src/components/text/Feedback.js
+++ src/components/text/Feedback.js
@@ -1,30 +1,57 @@
 import {css, html, LitElement} from 'lit';
 import '../commons/common.css';
+import {classMap} from "lit/directives/class-map.js";
 
 class LFeedback extends LitElement {
     static styles =
         [
             // component css
-             css`
-                 .is-valid~.valid-feedback, .is-valid~.valid-tooltip, .was-validated :valid~.valid-feedback, .was-validated :valid~.valid-tooltip {
-                     display: block;
-                 }
-                 
-                 .valid-feedback {
-                     display: block;
-                     width: 100%;
-                     margin-top: .25rem;
-                     font-size: .875em;
-                 }
-                 
-                 .invalid-feedback {
-                    display: block;
-                    width: 100%;
-                    margin-top: .25rem;
-                    font-size: .875em;
-                    color: var(--bs-danger);
-                 }
-                 
+            css`
+              .is-valid ~ .valid-feedback, .is-valid ~ .valid-tooltip, .was-validated :valid ~ .valid-feedback, .was-validated :valid ~ .valid-tooltip {
+                display: block;
+              }
+
+              .valid-feedback {
+                display: block;
+                width: 100%;
+                margin-top: .25rem;
+                font-size: .875em;
+              }
+
+              .invalid-feedback {
+                display: block;
+                width: 100%;
+                margin-top: .25rem;
+                font-size: .875em;
+                color: var(--bs-danger);
+              }
+
+              .form-group {
+                display: flex;
+                flex-direction: column;
+                gap: var(--krds-spacer-3);
+                width: 100%;
+              }
+
+              .form-group [class^=form-hint] {
+                color: var(--krds-gray-70);
+                padding-left: calc(2rem + var(--krds-spacer-1));
+              }
+
+              .form-group [class^=form-hint]::before {
+                display: inline-flex;
+                width: 1.2rem;
+                height: 1.2rem;
+                flex-shrink: 0;
+                flex-grow: 0;
+                content: "";
+                margin-left: calc((2rem + var(--krds-spacer-1)) * -1);
+                vertical-align: top;
+
+                background: url(/assets/ico_hint_20.svg) no-repeat center;
+                background-size: contain;
+              }
+
             `
         ]
     ;
@@ -43,12 +70,24 @@ class LFeedback extends LitElement {
         let isLabelLeft = (this.labelAlign && this.labelAlign == 'left');
         return html`
             <div class="valid-feedback"
-                style="
+                 style="
                     padding-left: calc(${this.leftMargin} + ${isLabelLeft ? `15px` : '0px'})
                     ;width: ${this.width ? this.width : 'auto'};
                 "
             >
-                ${this.feedback}
+                <div class="form-group">
+                    <div class="${
+                            classMap({
+                                'form-hint': this.feedbackType === 'hint',
+                                'invalid-feedback': this.feedbackType === 'error'
+                            })
+                    }
+                    ">
+                        ${this.feedback}
+                    </div>
+                </div>
+
+
             </div>
         `
     }
