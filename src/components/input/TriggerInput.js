77d6add4c183713b729dadaeb75c83824693dbfd
bhcha <oneforall88@gmail.com>
2025-06-09 22:24:54 +0900

    refactor: remove magnifier icon logic for disabled trigger input fix: update invalid SVG rendering issue

--- src/components/input/TriggerInput.js
+++ src/components/input/TriggerInput.js
@@ -42,6 +42,15 @@ class TriggerInput extends LabelAndFeedContainer {
                 cursor: pointer;
                 pointer-events: auto;
             }
+
+            .form-control.is-invalid, .was-validated .form-control:invalid {
+                border-color: var(--bs-danger);
+                padding-right: calc(1.5em + .75rem);
+                background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 12 12' width='12' height='12' fill='none' stroke='%23df1414'%3e%3ccircle cx='6' cy='6' r='4.5'/%3e%3cpath stroke-linejoin='round' d='M5.8 3.6h.4L6 6.5z'/%3e%3ccircle cx='6' cy='8.2' r='.6' fill='%23df1414' stroke='none'/%3e%3c/svg%3e");
+                background-repeat: no-repeat;
+                background-position: right calc(.375em + .1875rem) center;
+                background-size: calc(3.00em + .375rem) calc(.75em + .375rem)
+            }
         `
     ];
 
@@ -62,6 +71,9 @@ class TriggerInput extends LabelAndFeedContainer {
 
     _handleSearchClick(event) {
         this.triggerPop();
+    }
+
+    _nothing(event) {
     }
 
 
@@ -159,12 +171,10 @@ class TriggerInput extends LabelAndFeedContainer {
                             @keyup="${(this['valid-length-type'] != 'byte' ? null : super.createChangeHandler(ifDefined(this['maxlength']))) ?? nothing}"
                             autocomplete="${ifDefined(this['autocomplete'])}"
                     >
-                    ${isSearchRight ? html
-                            `
-                                <div @click="${this._handleSearchClick}"
-                                     class="search-icon-right ${this.value ? '' : 'hidden'}"
-                                     id="rightIcon"></div>` : nothing
-                    }
+                    <div @click="${isSearchRight ? this._handleSearchClick : this._nothing}"
+                         class="search-icon-right ${this.value ? '' : 'hidden'}"
+                         id="rightIcon"></div>
+                    
                 </div>
 
             </l-label-feed-container>
--- src/styles/SharedStyles.js
+++ src/styles/SharedStyles.js
@@ -1398,8 +1398,7 @@ class SharedStyles {
                 background-color: var(--bs-danger);
                 border-radius: var(--bs-border-radius)
             }
-
-
+            
             .form-control.is-invalid, .was-validated .form-control:invalid {
                 border-color: var(--bs-danger);
                 padding-right: calc(1.5em + .75rem);
@@ -1408,6 +1407,7 @@ class SharedStyles {
                 background-position: right calc(.375em + .1875rem) center;
                 background-size: calc(.75em + .375rem) calc(.75em + .375rem)
             }
+            
 
             .form-control.is-invalid:focus, .was-validated .form-control:invalid:focus {
                 border-color: var(--bs-danger);
